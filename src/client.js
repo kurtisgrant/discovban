95e4003ba22794afc5523d795bd964fe3efd4ea2
kurtisbrandon <kurtis.b.grant@gmail.com>
2021-05-25 18:44:32 -0400

    Added sanity apiversion

--- src/client.js
+++ src/client.js
@@ -4,5 +4,6 @@ import auth from './sanityAuth'
 export default sanityClient({
   projectId: auth.projectId,
   dataset: auth.dataset,
+  apiVersion: auth.apiVersion,
   useCdn: false
 })
--- src/sanityAuth.js
+++ src/sanityAuth.js
@@ -1,6 +1,7 @@
 const sanityAuth = {
   projectId: "q72pygsu",
   dataset: "production",
+  apiVersion: '2021-05-25',
 }
 
 export default sanityAuth
